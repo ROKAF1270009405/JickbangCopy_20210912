fcc6e88eda4a64c2ae24529d714ce10b8b1e90f2
전상효 <junsanghyo0820@gmail.com>
2021-09-12 14:39:32 +0900

    방 상세 정보 UI 수정

---
+++ app/src/main/java/com/neppplus/jickbangcopy_20210912/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.neppplus.jickbangcopy_20210912.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
